0fc1cf2042dcf3ce5a282bb9c43db0b4c3fa0855
Haletran <bpasquier123@gmail.com>
2024-09-03 14:02:30 +0200

    init: ex03 CPP04

---
+++ CPP04/ex03/main.cpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bapasqui <bapasqui@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/02 11:12:33 by bapasqui          #+#    #+#             */
+/*   Updated: 2024/09/02 12:20:06 by bapasqui         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Cure.hpp"
+#include "Ice.hpp"
+#include "IMateria_Source.hpp"
+#include "ICharacter.hpp"
+#include "AMateria.hpp"
+
+int main()
+{
+    IMateriaSource* src = new MateriaSource();
+    src->learnMateria(new Ice());
+    src->learnMateria(new Cure());
+
+    ICharacter* me = new Character("me");
+
+    AMateria* tmp;
+    tmp = src->createMateria("ice");
+    me->equip(tmp);
+    tmp = src->createMateria("cure");
+    me->equip(tmp);
+
+    ICharacter* bob = new Character("bob");
+    me->use(0, *bob);
+    me->use(1, *bob);
+
+    delete bob;
+    delete me;
+    delete src;
+    return 0;
+}
