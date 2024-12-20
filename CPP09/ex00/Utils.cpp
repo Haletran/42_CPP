97847d3a012ff9d1d37c43f0b463d41e76547cee
Haletran <bpasquier123@gmail.com>
2024-12-20 13:10:54 +0100

    fix: some stuf

--- CPP09/ex00/Utils.cpp
+++ CPP09/ex00/Utils.cpp
@@ -23,6 +23,7 @@ bool check_db_error(std::string line)
     if (regcomp(&parser, "^[0-9]{4}-[0-9]{2}-[0-9]{2},[0-9]+(\\.[0-9]+)?$", REG_EXTENDED) != 0)
     {
         std::cerr << BRED"Error: " RESET<< "Could not compile regex" << std::endl;
+        regfree(&parser);
         return (true);
     }
     if (regexec(&parser, line.c_str(), 0, NULL, 0) != 0)
@@ -35,6 +36,18 @@ bool check_db_error(std::string line)
     return (false);
 }
 
+bool check_if_digit(const char *str)
+{
+    int i = 0;
+    while (str[i])
+    {
+        if (!isdigit(str[i]) && str[i] != ' ' && str[i] != '.')
+            return (0);
+        i++;
+    }
+    return (1);
+}
+
 bool check_input_file_error(std::string line)
 {
     regex_t date;
@@ -43,18 +56,28 @@ bool check_input_file_error(std::string line)
     if (regexec(&date, line.substr(0, 10).c_str(), 0, NULL, 0) != 0)
     {
         std::cerr << BRED"Error : " RESET<< "bad input => " << line.substr(0, 10) << std::endl;
+        regfree(&date);
         return (true);
     }
     if (line.length() >= 12 && std::strtof(line.substr(12).c_str(), NULL) < 0)
     {
         std::cerr << BRED"Error : " RESET<< "not a positive number" << std::endl;
+        regfree(&date);
         return (true);
     }
     if (line.length() >= 12 && std::strtod(line.substr(12).c_str(), NULL) > 1000)
     {
         std::cerr << BRED"Error :" RESET<< " too large a number" << std::endl;
+        regfree(&date);
         return (true);
     }
+    if (!check_if_digit(line.substr(12).c_str()))
+    {
+        std::cerr << BRED"Error :" RESET<< " Invalid input" << std::endl;
+        regfree(&date);
+        return (true);
+    }
+    regfree(&date);
     int year = atoi(line.substr(0, 4).c_str());
     if (!(year % 4) && (year % 100 || !(year % 400)))
     {
@@ -69,6 +92,5 @@ bool check_input_file_error(std::string line)
         std::cerr << BRED"Error :" RESET<< " not a leap year" << std::endl;
         return (true);
     }
-    regfree(&date);
     return (false);
 }
